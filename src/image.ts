8cd600a8788f7a5ea2c3dd8bb3402d5aed9970c6
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-30 17:47:15 +0200

    fix: do not resolve avatar for private sponsors

--- src/image.ts
+++ src/image.ts
@@ -5,22 +5,42 @@ import sharp from 'sharp'
 import { consola } from 'consola'
 import type { SponsorkitConfig, Sponsorship } from './types'
 
-export async function resolveAvatars(ships: Sponsorship[], fallbackAvatar: SponsorkitConfig['fallbackAvatar'], t = consola) {
+export async function resolveAvatars(
+  ships: Sponsorship[],
+  getFallbackAvatar: SponsorkitConfig['fallbackAvatar'],
+  t = consola,
+) {
+  const fallbackAvatar = await (() => {
+    if (typeof getFallbackAvatar === 'string')
+      return $fetch(getFallbackAvatar, { responseType: 'arrayBuffer' })
+    if (getFallbackAvatar)
+      return getFallbackAvatar
+    return undefined
+  })()
+
+  const fallbackDataUri = fallbackAvatar && pngToDataUri(await round(fallbackAvatar, 0.5, 100))
+
   return Promise.all(ships.map(async (ship) => {
-    const data = await $fetch(ship.sponsor.avatarUrl, { responseType: 'arrayBuffer' })
-      .catch((e) => {
-        t.error(`Failed to fetch avatar for ${ship.sponsor.login || ship.sponsor.name} [${ship.sponsor.avatarUrl}]`)
-        t.error(e)
-        if (typeof fallbackAvatar === 'string')
-          return $fetch(fallbackAvatar, { responseType: 'arrayBuffer' })
-        if (fallbackAvatar)
-          return fallbackAvatar
-        throw e
-      })
-    const radius = ship.sponsor.type === 'User' ? 0.5 : 0.15
-    ship.sponsor.avatarUrlHighRes = pngToDataUri(await round(data, radius, 120))
-    ship.sponsor.avatarUrlMediumRes = pngToDataUri(await round(data, radius, 80))
-    ship.sponsor.avatarUrlLowRes = pngToDataUri(await round(data, radius, 50))
+    const data = ship.privacyLevel === 'PRIVATE'
+      ? fallbackAvatar
+      : await $fetch(ship.sponsor.avatarUrl, { responseType: 'arrayBuffer' })
+        .catch((e) => {
+          t.error(`Failed to fetch avatar for ${ship.sponsor.login || ship.sponsor.name} [${ship.sponsor.avatarUrl}]`)
+          t.error(e)
+          if (fallbackAvatar)
+            return fallbackAvatar
+          throw e
+        })
+
+    if (ship.privacyLevel === 'PRIVATE' && fallbackDataUri)
+      ship.sponsor.avatarUrl = fallbackDataUri
+
+    if (data) {
+      const radius = ship.sponsor.type === 'Organization' ? 0.1 : 0.5
+      ship.sponsor.avatarUrlHighRes = pngToDataUri(await round(data, radius, 120))
+      ship.sponsor.avatarUrlMediumRes = pngToDataUri(await round(data, radius, 80))
+      ship.sponsor.avatarUrlLowRes = pngToDataUri(await round(data, radius, 50))
+    }
   }))
 }
 
