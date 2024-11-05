c75e9db5d2e3f043b19ec0ba0d24e05d6ebf50f8
Hiroki Osame <hiroki.osame@gmail.com>
2024-11-06 08:50:03 +0900

    perf: store images as buffer instead of arraybuffer

--- src/processing/image.ts
+++ src/processing/image.ts
@@ -10,9 +10,11 @@ export async function resolveAvatars(
   getFallbackAvatar: SponsorkitConfig['fallbackAvatar'],
   t = consola,
 ) {
-  const fallbackAvatar = await (() => {
-    if (typeof getFallbackAvatar === 'string')
-      return $fetch(getFallbackAvatar, { responseType: 'arrayBuffer' })
+  const fallbackAvatar = await (async () => {
+    if (typeof getFallbackAvatar === 'string') {
+      const data = await $fetch(getFallbackAvatar, { responseType: 'arrayBuffer' })
+      return Buffer.from(data)
+    }
     if (getFallbackAvatar)
       return getFallbackAvatar
     return undefined
@@ -24,7 +26,7 @@ export async function resolveAvatars(
   const limit = pLimit(15)
 
   return Promise.all(ships.map(ship => limit(async () => {
-    const data = (ship.privacyLevel === 'PRIVATE' || !ship.sponsor.avatarUrl)
+    const pngArrayBuffer = (ship.privacyLevel === 'PRIVATE' || !ship.sponsor.avatarUrl)
       ? fallbackAvatar
       : await $fetch(ship.sponsor.avatarUrl, {
         responseType: 'arrayBuffer',
@@ -43,16 +45,17 @@ export async function resolveAvatars(
     if (ship.privacyLevel === 'PRIVATE' && fallbackDataUri)
       ship.sponsor.avatarUrl = fallbackDataUri
 
-    if (data) {
+    if (pngArrayBuffer) {
+      const pngBuffer = Buffer.from(pngArrayBuffer)
       const radius = ship.sponsor.type === 'Organization' ? 0.1 : 0.5
       const [
         highRes,
         mediumRes,
         lowRes,
       ] = await Promise.all([
-        round(data, radius, 120),
-        round(data, radius, 80),
-        round(data, radius, 50),
+        round(pngBuffer, radius, 120),
+        round(pngBuffer, radius, 80),
+        round(pngBuffer, radius, 50),
       ])
 
       const highResBase64 = highRes.toString('base64')
@@ -65,40 +68,12 @@ export async function resolveAvatars(
   })))
 }
 
-function toBuffer(ab: ArrayBuffer) {
-  const buf = Buffer.alloc(ab.byteLength)
-  const view = new Uint8Array(ab)
-  for (let i = 0; i < buf.length; ++i)
-    buf[i] = view[i]
-
-  return buf
-}
-
-export function base64ToArrayBuffer(base64: string) {
-  const binaryString = atob(base64)
-  const len = binaryString.length
-  const bytes = new Uint8Array(len)
-  for (let i = 0; i < len; i++)
-    bytes[i] = binaryString.charCodeAt(i)
-
-  return bytes.buffer
-}
-
-export function arrayBufferToBase64(buffer: ArrayBuffer) {
-  let binary = ''
-  const bytes = new Uint8Array(buffer)
-  const len = bytes.byteLength
-  for (let i = 0; i < len; i++)
-    binary += String.fromCharCode(bytes[i])
-  return btoa(binary)
-}
-
-export async function round(image: string | ArrayBuffer, radius = 0.5, size = 100) {
+export async function round(image: Buffer, radius = 0.5, size = 100) {
   const rect = Buffer.from(
     `<svg><rect x="0" y="0" width="${size}" height="${size}" rx="${size * radius}" ry="${size * radius}"/></svg>`,
   )
 
-  return await sharp(typeof image === 'string' ? image : toBuffer(image))
+  return await sharp(image)
     .resize(size, size, { fit: sharp.fit.cover })
     .composite([{
       blend: 'dest-in',
--- src/renders/circles.ts
+++ src/renders/circles.ts
@@ -1,4 +1,5 @@
-import { base64ToArrayBuffer, round } from '../processing/image'
+import { Buffer } from 'node:buffer'
+import { round } from '../processing/image'
 import { generateBadge, SvgComposer } from '../processing/svg'
 import type { Sponsor, SponsorkitRenderer, Sponsorship } from '../types'
 
@@ -66,7 +67,7 @@ async function getRoundedAvatars(sponsor: Sponsor) {
   if (!sponsor.avatarBuffer || sponsor.type === 'User')
     return sponsor
 
-  const data = base64ToArrayBuffer(sponsor.avatarBuffer)
+  const data = Buffer.from(sponsor.avatarBuffer, 'base64')
   const [
     highRes,
     mediumRes,
