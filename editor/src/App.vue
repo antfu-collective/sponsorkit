7dcf85ac0948acc31897b13c5bbd09a9c948ca70
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-31 14:54:30 +0900

    wip

---
+++ editor/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import data from '../../sponsorkit/sponsors.json'
+// import {} from
+
+// const sponsors = data as Sponsor
+</script>
+
+<template>
+  <div>{{ data }}</div>
+</template>
